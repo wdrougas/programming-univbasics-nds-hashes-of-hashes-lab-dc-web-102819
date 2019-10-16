4dd857b9d970b2657d0eda9f849acc030481f585
Will Drougas <wdrougas@gmail.com>
2019-10-16 00:31:07 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,8 +32,22 @@
 
 def naming_system
   {
-    label: "Kingdom" {
-      sub_category: 
+    label: "Kingdom",
+      sub_category: {
+        label: "Phylum",
+          sub_category: {
+            label: "Class",
+              sub_category: {
+                label: "Order",
+                  sub_category: {
+                    label: "Family",
+                      sub_category: {
+                        label: 
+                      }
+                  }
+              }
+          }
+      }
     }
  
 end
