2feed7f494b47151846215236e7efcab8e8c1b7d
Will Drougas <wdrougas@gmail.com>
2019-10-16 00:33:09 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,12 +45,5 @@ def naming_system
                         label: "Genus"
                           sub_category: {
                             label: "Species"
-                              sub_category: nil
-                          
-                      
-                  
-              
-          
-      
-    
+                              sub_category: nil}}}}}}}
 end
